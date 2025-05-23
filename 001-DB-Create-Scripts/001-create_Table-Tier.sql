ba0eea5b99fda7d2a543cdf7e94e3b7c89f2d803
Rebekka Kupffer <r.kupffer@posteo.de>
2025-05-23 12:37:47 +0200

    Create tabel Tier

---
+++ 001-DB-Create-Scripts/001-create_Table-Tier.sql
@@ -0,0 +1,28 @@
+-- ==================================================================================
+-- Abschlussprojekt TierTaxi von Simone Moeller, Theresa Duerbeck und Rebekka Kupffer
+-- Informationen zu diesem Script: Tier Tabelle erstellen
+-- Authorin(-nen): Rebekka
+-- ==================================================================================
+
+USE [TierTaxi]
+GO
+
+/****** Object:  Table [dbo].[tb_Tier]    Script Date: 23.05.2025 12:35:55 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[tb_Tier](
+	[TierID] [int] NOT NULL,
+	[Tier] [nchar](10) NOT NULL,
+	[Tierart] [nchar](10) NOT NULL,
+ CONSTRAINT [PK_tb_Tier] PRIMARY KEY CLUSTERED 
+(
+	[TierID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+
