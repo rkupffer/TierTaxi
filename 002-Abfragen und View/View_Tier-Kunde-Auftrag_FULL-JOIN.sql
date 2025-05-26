02d107e0cc441a89d152c785a8bb5ced44ad1606
Rebekka Kupffer <r.kupffer@posteo.de>
2025-05-26 17:27:24 +0200

    add create view

--- 002-Abfragen und View/View_Tier-Kunde-Auftrag_FULL-JOIN.sql
+++ 002-Abfragen und View/View_Tier-Kunde-Auftrag_FULL-JOIN.sql
@@ -3,7 +3,16 @@
 USE [TierTaxi]
 GO
 
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE VIEW [dbo].[View_Tier-Auftraege]
+AS
 SELECT  dbo.tb_Kunden.KundenVorname, dbo.tb_Kunden.KundenNachname, dbo.tb_Tier.Tier AS 'TierName', dbo.tb_Auftraege.DatumUhrzeitStart, dbo.tb_Auftraege.DatumUhrzeitEnde
 FROM    dbo.tb_Kunden INNER JOIN
                  dbo.tb_Auftraege ON dbo.tb_Kunden.Kunden_ID = dbo.tb_Auftraege.Kunde_ID FULL OUTER JOIN
 		         dbo.tb_Tier ON dbo.tb_Auftraege.Tier_ID = dbo.tb_Tier.Tier_ID
+GO
--- 002-Abfragen und View/View_Tier-Standort_COUNT.sql
+++ 002-Abfragen und View/View_Tier-Standort_COUNT.sql
@@ -1,12 +1,20 @@
 -- Sicht ueber mehrere Tabellen mit Count
 -- Tiere an den verfuegbaren Standorten zaehlen
-
 USE [TierTaxi]
 GO
 
-SELECT   COUNT(dbo.tb_Tier.Tier_ID), dbo.tb_Standort.Standort
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE VIEW [dbo].[View_Count_Tier_per_Standort]
+AS
+SELECT   COUNT(dbo.tb_Tier.Tier_ID) AS 'Anzahl', dbo.tb_Standort.Standort
 FROM     dbo.tb_Tier INNER JOIN
                          dbo.tb_Tierart ON dbo.tb_Tier.Tierart_ID = dbo.tb_Tierart.Tierart_ID
 					INNER JOIN
                          dbo.tb_Standort ON dbo.tb_Tier.Standort_ID = dbo.tb_Standort.Standort_ID
 GROUP BY dbo.tb_Standort.Standort;
+GO
