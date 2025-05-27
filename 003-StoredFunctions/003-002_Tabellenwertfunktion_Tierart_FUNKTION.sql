7276426ea343594ea79b974cd9eb122e9b507ec2
TheresaAbb <59801657+TheresaAbb@users.noreply.github.com>
2025-05-27 10:05:06 +0200

    ersetzt

--- 003-StoredFunctions/003-002_Tabellenwertfunktion_Tierart_FUNKTION.sql
+++ 003-StoredFunctions/003-002_Tabellenwertfunktion_Tierart_FUNKTION.sql
@@ -10,17 +10,20 @@ GO
 -- =============================================
 CREATE OR ALTER FUNCTION tf_Futterpraeferenz
 (	
-	@TierID int
+	@Tierart nchar(20)
 )
 RETURNS TABLE 
 AS
 RETURN 
 (
-	SELECT		dbo.tb_Futtertyp.Futtertyp
-	FROM        dbo.tb_Tierart INNER JOIN
+	SELECT		dbo.tb_Tier.Tier_ID, 
+				dbo.tb_Tier.Tier, 
+				dbo.tb_Tierart.Tierart, 
+				dbo.tb_Futtertyp.Futtertyp
+	FROM         dbo.tb_Tierart INNER JOIN
                          dbo.tb_Tier ON dbo.tb_Tierart.Tierart_ID = dbo.tb_Tier.Tierart_ID INNER JOIN
                          dbo.tb_Futterkosten ON dbo.tb_Tierart.Futterkosten_ID = dbo.tb_Futterkosten.Futterkosten_ID INNER JOIN
                          dbo.tb_Futtertyp ON dbo.tb_Futterkosten.Futtertyp_ID = dbo.tb_Futtertyp.Futtertyp_ID
-	WHERE dbo.tb_Tier.Tier_ID = @TierID
+	WHERE dbo.tb_Tierart.Tierart = @Tierart
 )
 GO
