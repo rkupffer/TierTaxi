ad4961eb0274c84eaa3499ee4c1ff1143d926c12
Rebekka Kupffer <r.kupffer@posteo.de>
2025-05-27 16:01:34 +0200

    mit Test 4, TAP

--- 004-StoredProcedures/001-sp_Buchungsprozess_test_exec.sql
+++ 004-StoredProcedures/001-sp_Buchungsprozess_test_exec.sql
@@ -1,12 +1,12 @@
 USE TierTaxi
 GO
 
+-- ===================================================================================================
+-- Test1: Auftrag ablehenen, wenn am Standort kein Tier ist, dass das entsprechende Mitnahmegewicht leisten kann
+-- ===================================================================================================
 DECLARE	@Erfolg bit; -- geklappt oder nicht
 DECLARE	@Feedback NVARCHAR(MAX); -- Fehlermeldungen etc.
 
--- testen, ob man mit der systemprozedur daten in eine tabelle hinzufuegen kann
-
--- Test1: Auftrag ablehenen, wenn am Standort kein Tier ist, dass das entsprechende Mitnahmegewicht leisten kann
 EXEC [dbo].[sp_Buchungsprozess]
 	3, -- KundenID
 	----------------------- 
@@ -16,7 +16,12 @@ EXEC [dbo].[sp_Buchungsprozess]
 PRINT @Erfolg;
 PRINT @Feedback;
 
+-- ===================================================================================================
 -- Test2: Fehler, wenn der Kunde nicht exsistiert
+-- ===================================================================================================
+DECLARE	@Erfolg bit; -- geklappt oder nicht
+DECLARE	@Feedback NVARCHAR(MAX); -- Fehlermeldungen etc.
+
 EXEC [dbo].[sp_Buchungsprozess]
 	123, -- KundenID
 	----------------------- 
@@ -26,9 +31,30 @@ EXEC [dbo].[sp_Buchungsprozess]
 PRINT @Erfolg;
 PRINT @Feedback;
 
+-- ===================================================================================================
 -- Test3: Kunde exsistiert und am Standort gibt es ein geeignetes Tier
+-- ===================================================================================================
+DECLARE	@Erfolg bit; -- geklappt oder nicht
+DECLARE	@Feedback NVARCHAR(MAX); -- Fehlermeldungen etc.
+
 EXEC [dbo].[sp_Buchungsprozess]
 	1, -- KundenID
+	----------------------- 
+	@Erfolg OUTPUT,
+	@Feedback OUTPUT;
+
+PRINT @Erfolg;
+PRINT @Feedback;
+
+-- ===================================================================================================
+-- Test4: Kunde exsistiert und am Standort gibt es ein geeignetes Tier 
+	--> Tier hat aber kein gültiges TAP
+-- ===================================================================================================
+DECLARE	@Erfolg bit; -- geklappt oder nicht
+DECLARE	@Feedback NVARCHAR(MAX); -- Fehlermeldungen etc.
+
+EXEC [dbo].[sp_Buchungsprozess]
+	4, -- KundenID
 	----------------------- 
 	@Erfolg OUTPUT,
 	@Feedback OUTPUT;
